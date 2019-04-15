9922e95bd5ba6e386c3cf279a5050d162703e406
Guobei <guobei.jwh@alibaba-inc.com>
2019-04-15 09:03:38 +0800

    clean .doom.d config

--- packages.el
+++ packages.el
@@ -1,32 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
-;;; private/my/packages.el
+;;; ~/.doom.d/packages.el
 
-;; disabled packages
-(packages! (exec-path-from-shell :disable t)
-           (solaire-mode :disable t)
-           (cquery :disable t)
-           (anaconda-mode :disable t)
-           (company-anaconda :disable t))
-
-;; misc
-(packages! avy
-           edit-indirect
-           atomic-chrome
-           link-hint
-           symbol-overlay
-           tldr
-           (blog-admin :recipe (:fetcher github :repo "codefalling/blog-admin"))
-           youdao-dictionary
-           wucuo
-           org-wild-notifier
-           (vterm-toggle :recipe (:fetcher github :repo "jixiuf/vterm-toggle"))
-           )
-
-;; programming
-(packages! lsp-mode lsp-ui company-lsp
-           import-js indium
-           importmagic py-isort
-           rust-mode
-	   protobuf-mode
-           (flycheck-google-cpplint :recipe (:fetcher github :repo "flycheck/flycheck-google-cpplint"))
-           )
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
