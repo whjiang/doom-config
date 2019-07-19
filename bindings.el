600c4c906146621988cb1f100b7cc2e633e7ec60
Guobei <guobei.jwh@alibaba-inc.com>
2019-07-19 14:27:28 +0800

    enable menu bar for terminal

--- bindings.el
+++ bindings.el
@@ -6,7 +6,8 @@
     (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
     (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
     (setq mouse-wheel-up-event 'mouse-5)
-    (setq mouse-wheel-down-event 'mouse-4))
+    (setq mouse-wheel-down-event 'mouse-4)
+    (toggle-menu-bar-mode-from-frame))
 
 (xterm-mouse-mode -1)
 (setq mouse-drag-copy-region t)
